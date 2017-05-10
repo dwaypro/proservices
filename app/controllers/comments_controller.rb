e4b19285577dc23840b5e4691df80d76fd80a806
Ubuntu <ubuntu@ip-172-31-21-118.us-west-1.compute.internal>
2017-05-10 19:14:15 +0000

    adding all files

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+ http_basic_authenticate_with name: "dan", password: "dan", 
+except: [:index, :show]
+
+ def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+end
